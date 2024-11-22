0454b5f98088ad87f4c3544a880ecf036bf73263
frandvla <francisco.davila@destacados.cl>
2024-11-22 17:57:21 -0300

    Scroll hacia arriba al cambiar de página

--- src/resources/js/Components/PaginationControl/PaginationControl.jsx
+++ src/resources/js/Components/PaginationControl/PaginationControl.jsx
@@ -2,17 +2,22 @@ import { Pagination } from 'react-bootstrap';
 import styles from './PaginationControl.module.css'
 
 const PaginationControl = ({ totalPages, currentPage, setCurrentPage }) => {
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <Pagination className="justify-content-center mt-4">
-            <Pagination.First onClick={() => setCurrentPage(1)} disabled={currentPage === 1} />
-            <Pagination.Prev onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} disabled={currentPage === 1} />
+            <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
+            <Pagination.Prev onClick={() => handlePageChange(Math.max(currentPage - 1, 1))} disabled={currentPage === 1} />
             {[...Array(totalPages)].map((_, idx) => (
-                <Pagination.Item key={idx + 1} active={idx + 1 === currentPage} onClick={() => setCurrentPage(idx + 1)}>
+                <Pagination.Item key={idx + 1} active={idx + 1 === currentPage} onClick={() => handlePageChange(idx + 1)}>
                     {idx + 1}
                 </Pagination.Item>
             ))}
-            <Pagination.Next onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages} />
-            <Pagination.Last onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages} />
+            <Pagination.Next onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))} disabled={currentPage === totalPages} />
+            <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
         </Pagination>
     );
 };
