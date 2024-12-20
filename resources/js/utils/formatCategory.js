efd3684b81a2200b54e629c33fb5080c052c3335
frandvla <francisco.davila@destacados.cl>
2024-12-20 09:32:18 -0300

    fix replace de categoria

--- resources/js/utils/formatCategory.js
+++ resources/js/utils/formatCategory.js
@@ -1,3 +1,12 @@
 export const formatCategory = (category) => {
+    if (Array.isArray(category)) {
+        // Si es un array, aplica la función recursivamente a cada elemento
+        return category.map((item) => formatCategory(item));
+    }
+    if (typeof category !== "string") {
+        // Si no es un string, devuelve un string vacío
+        return "";
+    }
+    // Si es un string, aplica el reemplazo
     return category.replace("Suv / Station Wagon", "Suv");
 };
