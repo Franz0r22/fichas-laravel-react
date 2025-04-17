0d6f50383d9dd44d792260ca9bc3728213480f91
AGDluisPizarro <luis.pizarro@destacados.cl>
2025-04-17 17:40:11 -0400

    se agrega contador de visitas en ficha ampliada y obtencion de email de sucursal segun api

--- app/Http/Controllers/CarQuoteController.php
+++ app/Http/Controllers/CarQuoteController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Log;
 use App\Mail\QuoteMail;
 use App\Mail\ThankYouQuoteMail;
 use App\Rules\Recaptcha;
+use Illuminate\Support\Facades\Http;
 
 class CarQuoteController extends Controller
 {
@@ -37,15 +38,48 @@ class CarQuoteController extends Controller
             'kilometraje' => 'nullable|integer',
         ]);
 
+        $contactos = $this->getContactos();
+        $to = json_decode($contactos->getContent(), true);
+
+        $toDef = $request->all()['automotoraMail'] ?? 'diseno@destacados.cl';
+
         $this->GuardaCorreo($validated);
 
         // Enviar correo al administrador
-        Mail::to($validated['automotoraMail'])->cc('francisco.davila@destacados.cl')->send(new QuoteMail($validated));
+        Mail::to(explode(',', $to['data'] ?? $toDef))->send(new QuoteMail($validated));
 
         // Enviar correo de agradecimiento al usuario
         Mail::to($validated['email'])->send(new ThankYouQuoteMail($validated));
 
         return redirect()->back()->with('success', 'Cotización enviada con éxito');
+    }
+
+    public function getContactos()
+    {
+        $apiUrl = config('services.api.urlfichas');
+        $apiToken = config('services.api.tokenfichasv2');
+        $endpoint = '/diccionario/autos/sucursales/';
+        $url = $apiUrl . $endpoint . config('services.api.idClientes');
+
+        $response = Http::withToken($apiToken)->get($url);
+
+        if ($response->successful()) {
+            $info = $response->json();
+            $lastPosition = count($info) - 1;
+            $data = $info[$lastPosition];
+            return response()->json([
+                'message' => 'Contactos enviados',
+                'data' => $data,
+            ]);
+        } else {
+            return response()->json(
+                [
+                    'message' => 'Error al obtener contactos',
+                    'errors' => $response->json(),
+                ],
+                500,
+            );
+        }
     }
 
     protected function GuardaCorreo($lead)
